e041c022556c1a2f8ca0eff6a2fe90e9e4e6bc2b
Seb <bustosospinat@gmail.com>
2024-02-26 19:48:08 -0600

    Add node files

---
+++ src/TodoIcon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ReactComponent as CheckSVG } from './check.svg';
+import { ReactComponent as DeleteSVG } from './delete.svg';
+import './TodoIcon.css';
+
+const iconTypes = {
+	check: (color) => (
+		<CheckSVG
+			className="Icon-svg"
+			fill={color}
+		/>
+	),
+	delete: (color) => (
+		<DeleteSVG
+			className="Icon-svg"
+			fill={color}
+		/>
+	),
+};
+function TodoIcon({ type, color, onClick }) {
+	return (
+		<span
+			className={`Icon Icon-${type}`}
+			onClick={onClick}>
+			{iconTypes[type](color)}
+		</span>
+	);
+}
+
+export { TodoIcon };
